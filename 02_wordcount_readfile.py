5cb7320c31fe8b92797830185b64e29800ddf0a0
Azmi Kamis <azmikamis@outlook.com>
2018-11-08 23:06:36 +0800

    wordcount from file

---
+++ 02_wordcount_readfile.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import re
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+
+with beam.Pipeline() as p:
+    counts = (
+        p
+        | ReadFromText('input.txt')
+        | beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x)) # Find all word matches
+        | beam.Map(lambda x: (x, 1))                            # Create tuple (word,1)
+        | beam.CombinePerKey(sum)                               # Reduce by key i.e. the word
+        | beam.ParDo(lambda x: print(x))                        # Print output
+    )
